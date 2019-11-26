b8272d140c652831abe9b73e081d4ca111cff8a6
Юрасов Вячеслав <2665603@gmail.com>
2019-11-26 10:56:34 +0500

    Cahnge settings names of players
    Set Players from text.voxes in form1

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,6 @@ namespace TicTacToe_TheGame
 {
     public partial class Form1 : Form
     {
-        static string player1_name, player2_name;
         int x_win_count = 0, o_win_count = 0, draw_count = 0;
         bool turn = true; //ehen true - X turn, false - 0 turn
         int turnCount = 0;
@@ -17,10 +16,7 @@ namespace TicTacToe_TheGame
         }
         private void AboutMenuItem(object sender, EventArgs e) => MessageBox.Show("By SlavY Yurasov 2019", "Tic-Tac-Toe About");
         private void ExitMenuItem(object sender, EventArgs e) => Application.Exit();
-        public static void SetPlayersName(string n1, string n2)
-        {
-            player1_name = n1; player2_name = n2;
-        }
+
         private void Button_click(object sender, EventArgs e) //нажатие на любую кнопку в поле игры
         {
             turnCount++;
@@ -38,13 +34,13 @@ namespace TicTacToe_TheGame
 
                 if (turn) //выводим победителя
                 {
-                    MessageBox.Show($"{player1_name} Win!", "Yea!!");
-                    X_WinCount_label.Text = $"{player1_name} wins = {++x_win_count}";
+                    MessageBox.Show($"{p1_name.Text} Win!", "Yea!!");
+                    p1_score.Text = (++x_win_count).ToString();
                 }
                 else
                 {
-                    MessageBox.Show($"{player2_name} Win!", "Yea!");
-                    O_WinCount_label.Text = $"{player2_name} wins = {++o_win_count}";
+                    MessageBox.Show($"{p2_name.Text} Win!", "Yea!");
+                    p2_score.Text = (++o_win_count).ToString();
                 }
 
                 return;
@@ -71,22 +67,12 @@ namespace TicTacToe_TheGame
         {
             x_win_count = o_win_count = draw_count = 0;
             Draw_Count_label.Text = $"Draws = {draw_count}";
-            X_WinCount_label.Text = $"X wins = {x_win_count}";
-            O_WinCount_label.Text = $"O wins = {o_win_count}";
+            p1_score.Text = (x_win_count).ToString();
+            p2_score.Text = (o_win_count).ToString();
             //NewGame_Click(null, null);
             newGameToolStripMenuItem.PerformClick();
         }
 
-        private void Form_load_and_set_players_name(object sender, EventArgs e)
-        {
-            using (Form2 f = new Form2())
-            {
-                f.ShowDialog();
-            }
-            X_WinCount_label.Text = $"{player1_name} wins = 0";
-            O_WinCount_label.Text = $"{player2_name} wins = 0";
-            Draw_Count_label.Text = $"Draws = 0";
-        }
 
         private bool CheckForWinner()
         {
